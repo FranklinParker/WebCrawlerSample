422e9a08adb4ebf39dc43a1112aeac03be3ea467
Franklin E. Parker <franklinparker@Franklins-MBP-2.fios-router.home>
2018-02-13 11:54:58 -0500

    Parse different tags

--- app.js
+++ app.js
@@ -37,15 +37,43 @@ crawler.start();
 
 var processTerm = (html) => {
 	var termRecord = {};
+	console.log(html);
+	const td = '<TD><FONT FACE="Arial" COLOR="#FEFEEE" SIZE="5">';
+	const tdStart = html.indexOf(td)>-1 ? html.indexOf(td)+ td.length +1: -1;
+
+	const tdEnd = html.indexOf('</FONT></TD>');
+	if(tdEnd>tdStart && tdStart>-1){
+		const len = tdEnd - tdStart -1;
+		termRecord.name = html.substr(tdStart, len);
+	}
+
 
 	const h3 = '<H3>';
-	const h3Start = html.indexOf(h3) +4;
+	const h3Start = html.indexOf(h3)>-1?html.indexOf(h3) +4: -1;
 	const h3End =  html.indexOf('</H3>');
 	if(h3Start>-1 && h3End>h3Start){
 		const h3Len =  h3End - h3Start;
-		termRecord.term = h3Text;
 		const h3Text = html.substr(h3Start, h3Len);
+
 		termRecord.term = h3Text;
+	}
+
+
+	const p1 = '<P><FONT FACE="Arial" SIZE=2>';
+	const p1Start = html.indexOf(p1)>-1?html.indexOf(p1) + p1.length: -1;
+	const p1End =  html.indexOf('</FONT></P>');
+	if(p1Start>-1 && p1Start>p1End){
+		const len =  p1End - p1Start;
+		termRecord.shortDefinition  = html.substr(p1Start,len);
+	}
+
+
+	const p2 = '<P><FONT FACE="Arial" SIZE=2>';
+	const p2Start = html.indexOf(p1End,p2)>-1?html.indexOf(p1End,p2) + p1.length: -1;
+	const p2End =  html.indexOf(p1End + '</FONT></P>'.length,'</FONT></P>');
+	if(p2Start>-1 && p2Start>p2End){
+		const len =  p2End - p2Start;
+		termRecord.longDefinition  = html.substr(p2Start,len);
 	}
 
 	console.log('term record', termRecord);
