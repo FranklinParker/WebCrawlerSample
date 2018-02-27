4ed0e275836576a55386c68f0c00073447f7453e
Franklin E. Parker <franklinparker@Franklins-MBP-2.fios-router.home>
2018-02-26 20:39:32 -0500

    Use

--- SapGlossaryClient/src/app/customer/service/customer.service.ts
+++ SapGlossaryClient/src/app/customer/service/customer.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 import {Observable} from "rxjs/Observable";
@@ -11,22 +11,42 @@ export class CustomerService {
   urlFunctionalAssessment = environment.serverUrl + 'api/functionalAssessments';
 
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   getAllCustomers(): Observable<Customer[]> {
     return this.http.get(this.url + '/getAll')
-      .map((response:{ status:string, records: Customer[]})=>{
+      .map((response: { status: string, records: Customer[] }) => {
         return response.records;
       });
   }
 
   getAllFunctionalAssessments(): Observable<FunctionalAssessment[]> {
     return this.http.get(this.urlFunctionalAssessment + '/getAll')
-      .map((response:{ status:string, records: FunctionalAssessment[]})=>{
-        return response.records;
+      .map((response: { status: string, response }) => {
+
+        const funcAssessments: FunctionalAssessment[] = [];
+        response['records'].forEach((rec) => {
+          funcAssessments.push({
+            id: rec['id'],
+            customerId: rec['customer_id'],
+            ProcesGroupL1: rec['process_group_l1'],
+            Module: rec['module'],
+            ProcessScenarioL2: rec['process_scenario_l2'],
+            ProcessComponentL3: rec['module'],
+            ProcessComponentL4: rec['process_component_l3'],
+            ProcessComponentL5: rec['process_component_l4'],
+            S4HANAImpact: rec['s4hana_impact'],
+            DefaultStatus: rec['default_status'],
+            FinalStatus: rec['final_status'],
+            OverviewChangeSAPS4HANA: rec['overview_change'],
+            ChangeImpactBasedOnSystemAnalysis: rec['change_impact_system_analysis'],
+            ProcessGroup: rec['process_group'],
+            ImpactedTCode: rec['impacted_tcode']
+          });
+        });
+        return funcAssessments;
       });
   }
-
-
 
 }
--- nodeserver/database/sapGlossSql.js
+++ nodeserver/database/sapGlossSql.js
@@ -1,7 +1,21 @@
 const Sequelize = require('sequelize');
 
-const sequelize = require('./connectionPostGres').connection;
 const Op = Sequelize.Op;
+
+const sequelize = new Sequelize('compose', 'admin', 'ARJKDCRVIPIGPLAG', {
+	host: 'sl-us-south-1-portal.20.dblayer.com',
+	dialect: 'postgres',
+	port: 34751,
+	logging: false,
+
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 30000,
+		idle: 10000
+	},
+	operatorsAliases: false
+});
 
 
 const SapGlossary = sequelize.define('SapGlossary', {
