3364fe9d6381af9db3b80c663748af62e167d1f4
Franklin E. Parker <franklinparker@Franklins-MBP-2.fios-router.home>
2018-02-27 14:32:52 -0500

    View

--- nodeserver/database/functionalAssessmentView.js
+++ nodeserver/database/functionalAssessmentView.js
@@ -1,16 +1,23 @@
 const Sequelize = require('sequelize');
+const fs = require('fs');
 const sequelize = require('./connectionPostGres').connection;
 
 
 const saveOutFunctionalAssessmentToFile = (fileOut) => {
-	sequelize.query("select * from s4hana.customer c, s4hana.out_bm_func_assess out_fa where c.id = out_fa.customer_id LIMIT 20"
+	sequelize.query(
+		"select * from s4hana.customer c, s4hana.out_bm_func_assess out_fa where c.id = out_fa.customer_id"
 		, {type: sequelize.QueryTypes.SELECT})
 		.then((functionalAss) => {
-			console.log(JSON.stringify(functionalAss,null,2));
+
+			console.log("number records:" + functionalAss.length);
+			fs.appendFile(fileOut, JSON.stringify(functionalAss,null,2), (err) => {
+				if (err) throw err;
+				console.log('The "data to append" was appended to file!');
+			});
 
 
 		});
 }
 
 
-saveOutFunctionalAssessmentToFile('');
+saveOutFunctionalAssessmentToFile('/Users/franklinparker/jsonForWatson/customerFuncAssessment.json');
