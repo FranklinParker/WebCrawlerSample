396f08f5b8130fd259a9c66debe569b973abf5ac
Franklin E. Parker <parkerf@us.ibm.com>
2018-02-27 22:07:00 -0500

    Update readme.cmd

--- nodeserver/readme.cmd
+++ nodeserver/readme.cmd
@@ -1,5 +1,5 @@
 deploy to bluemix
 
-C:\hiacode\HiaApp>cf target -s dev
+$cf target -s dev
 
-C:\hiacode\HiaApp>cf push SAPGlossary -m 1536M --hostname sapglossary-dev -d w3ibm.mybluemix.net
+$cf push SAPGlossary -m 1536M --hostname sapglossary-dev -d w3ibm.mybluemix.net
